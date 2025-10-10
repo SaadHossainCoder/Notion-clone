fa58bba46f33f4d776a46d35020ec1bce66080ae
Saad Hossain <hossainsaad324@gmail.com>
2025-10-10 19:51:54 +0530

    Refactor document queries and navigation components; comment out unused code for clarity

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -5,7 +5,7 @@ import { usePathname } from "next/navigation";
 import { useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { UserItem } from "./user-item";
-import { useMutation, useQuery } from "convex/react";
+import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { Item } from "./item";
 import { toast } from "sonner";
@@ -13,7 +13,7 @@ import { toast } from "sonner";
 const Navigation = () => {
     const pathname = usePathname();
     const isMobile = useMediaQuery("(max-width: 768px)");
-    const documents = useQuery(api.documents.getUserDocuments) || [];
+    // const documents = useQuery(api.documents.getUserDocuments) || [];
 
     const create = useMutation(api.documents.createDocument);
 
@@ -153,11 +153,11 @@ const Navigation = () => {
 
                 <div className="mt-4">
                     {
-                        documents.map((document) => (
-                            <p key={document._id}>
-                                {document.title}
-                            </p>
-                        ))
+                        // documents.map((document) => (
+                        //     <p key={document._id}>
+                        //         {document.title}
+                        //     </p>
+                        // ))
                     }
                 </div>
 
